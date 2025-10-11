c67581a86dfdbb3460939579169a98a6650013b8
Francis Miguel Castillo Cruz <franciscastillodev@proton.me>
2025-10-11 17:36:26 -0400

    feat: enhance dashboard layout and add new features
    - Updated the dashboard header with a new title and description for better user guidance.
    - Introduced a button for creating new wishlists, improving user interaction.
    - Added floating animation to enhance visual appeal.
    - Integrated new feature images for dark and light themes in the features section.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/layout/navbar";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 import React from "react";
 
 const page = () => {
@@ -6,7 +8,21 @@ const page = () => {
     <div className="min-h-screen">
       <Navbar />
       <div className="container mx-auto px-4 pt-24 pb-12">
-        <h1 className="text-4xl md:text-5xl font-bold mb-3">Mis wishlists</h1>
+        {/* Header */}
+        <div className="flex flex-col md:flex-row md:items-center justify-between gap-6 mb-12 animate-fade-in">
+          <div>
+            <h1 className="text-4xl md:text-5xl font-bold mb-3">
+              Mis wishlists
+            </h1>
+            <p className="text-lg text-muted-foreground">
+              Gestiona todas tus listas de deseos en un solo lugar
+            </p>
+          </div>
+          <Button size={"lg"} variant={"secondary"} className="group">
+            <Plus className="size-5 mr-2 group-hover:rotate-90 transition-transform" />
+            Nueva Wishlist
+          </Button>
+        </div>
       </div>
     </div>
   );
--- components/layout/features-section.tsx
+++ components/layout/features-section.tsx
@@ -1,6 +1,8 @@
 import { Share2, Sparkles, Heart, Palette } from "lucide-react";
 import Image from "next/image";
 import { TextEffect } from "../motion-primitives/text-effect";
+import featureLight from "@/assets/feature-light.png";
+import featureDark from "@/assets/feature-dark.png";
 
 export default function FeaturesSection() {
   return (
@@ -42,14 +44,14 @@ export default function FeaturesSection() {
           <div className="border-border/50 relative rounded-3xl border p-3 lg:col-span-3">
             <div className="bg-linear-to-b aspect-76/59 relative rounded-2xl from-zinc-300 to-transparent p-px dark:from-zinc-700">
               <Image
-                src="/payments.png"
+                src={featureDark}
                 className="hidden rounded-[15px] dark:block"
                 alt="payments illustration dark"
                 width={1207}
                 height={929}
               />
               <Image
-                src="/payments-light.png"
+                src={featureLight}
                 className="rounded-[15px] shadow dark:hidden"
                 alt="payments illustration light"
                 width={1207}
